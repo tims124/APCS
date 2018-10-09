c0f8781fd6912a5ab6f0f1039037a2050daad4a2
tims124 <43677159+tims124@users.noreply.github.com>
2018-10-09 10:34:07 -0600

    stuff

--- lab_3/Fraction.java
+++ lab_3/Fraction.java
@@ -9,25 +9,43 @@ public class Fraction{
 		num = n;
 		denom = 1;
 	}
-	
+
 	public int getNum(){
 	return num;
 	}
-	
+
 	public int getDenom(){
 	return denom;
 	}
-	
+
 	public Fraction multiplyFraction(Fraction a){ //multiplies the original fraction by a new fraction
 	denom = this.getDenom() * a.getDenom()	;
 	num =this.getNum( )* a.getNum();
 	return new Fraction(num,denom); // then returns it
 	}
-	
+
+	public Fraction divideFraction(Fraction a){
+		this.denom = this.getDenom() * a.getNum();
+		this.num = this.getNum() * a.getDenom();
+		return new Fraction(this.num,this.denom);
+	}
+
+	public Fraction addFraction(Fraction a){
+		this.num = this.getNum * a.getDenom + this.getDenom * a.getNum;
+		this.denom =this.denom * a.denom;
+		return new Fraction(this.num,this.denom);
+	}
+
+	public Fraction subtractFraction(Fraction a){
+		this.num = this.getNum * a.getDenom - this.getDenom * a.getNum;
+		this.denom =this.denom * a.denom;
+		return new Fraction(this.num,this.denom);
+	}
+
 	public void dispFraction(){
 	System.out.println(num+"/"+denom);
 	}
-		
+
 	private int num;
 	private int denom;
 }
--- lab_3/FractionTest.java
+++ lab_3/FractionTest.java
@@ -1,11 +1,27 @@
 /**
 Enderton's class to test the Fraction class
+Also, extend the test program to ask for user input for the numerator and denominator of
+both the first and second fractions.
 */
+import java.util.Scanner;
 public class FractionTest{
 	public static void main(String[] args){
-	
-	Fraction f1 = new Fraction(2,5);
-	Fraction f2 = new Fraction(3,4);
+
+	Scanner input = new Scanner(System.in)
+	System.out.println("Enter denominator of the first number: ");
+	int d1 = input.nextInt();
+
+	System.out.println("Enter numerator of the first number: ");
+	int n1 = input.nextInt();
+
+	System.out.println("Enter denominator of the second number: ");
+	int d2 = input.nextInt();
+
+	System.out.println("Enter numerator of the second number: ");
+	int n2 = input.nextInt();
+
+	Fraction f1 = new Fraction(n1,d1);
+	Fraction f2 = new Fraction(n2,d2);
 	Fraction r = new Fraction(1,1);
 	r = f1.multiplyFraction(f2);
 	r.dispFraction();
