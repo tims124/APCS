512d61339c7bf0da331181d5cb8ab4c97bcce849
tims124 <43677159+tims124@users.noreply.github.com>
2018-10-16 13:11:05 -0600

    asdf

---
+++ lab_0/TruthTables.java
@@ -0,0 +1,21 @@
+public class TruthTables {
+   public static void main(String[] args) {
+     boolean pt = true;
+     boolean pf = false;
+     boolean qt = true;
+     boolean qf = true;
+
+     boolean test = (pt && qf);
+     System.out.println(test);
+     boolean a = ((pt && qt) || (pt == false));
+     boolean b = ((pt && qf) || (pt == false));
+     boolean c = ((pf && qt) || (pf == false));
+     boolean d = ((pf && qf) || (pf == false));
+
+     System.out.println("T-T: "+a);
+     System.out.println("T-F: "+b);
+     System.out.println("F-T: "+c);
+     System.out.println("F-F: "+d);
+
+   }
+}
