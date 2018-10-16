2ae955318c935356c5d15d076ddf3a700ea8441f
tims124 <43677159+tims124@users.noreply.github.com>
2018-10-16 11:21:23 -0600

    test

--- lab_0/HelloWorld.java
+++ lab_0/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello World");
+	   final int num1 = 100;
+	   final double num2 = 123.321;
+      System.out.println(num1 + ""+num2);
+	  num1 = 2;
    }
 }
