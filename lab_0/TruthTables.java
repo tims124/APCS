d6ea6e1f4f2b9a8cbef2c8eee72f4e1a309e3e2d
tims124 <43677159+tims124@users.noreply.github.com>
2018-10-17 11:52:52 -0600

    truth tables

--- lab_0/TruthTables.java
+++ lab_0/TruthTables.java
@@ -15,10 +15,33 @@ public class TruthTables {
      System.out.println("F-T: "+c);
      System.out.println("F-F: "+d);
 
-     boolean a = ((pt && qt) || (pt == false));
-     boolean b = ((pt && qf) || (pt == false));
-     boolean c = ((pf && qt) || (pf == false));
-     boolean d = ((pf && qf) || (pf == false));
+
+     a = ((pt == false) || qt) || (pt && qt);
+     b = ((pt == false) || qf) || (pt && qf);
+     c = ((pf == false) || qt) || (pf && qt);
+     d = ((pf == false) || qf) || (pf && qf);
+
+     System.out.println("T-T: "+a);
+     System.out.println("T-F: "+b);
+     System.out.println("F-T: "+c);
+     System.out.println("F-F: "+d);
+
+
+     a = ((pt || qt) && (pt && qt)) || ((pt == false && qt == false)
+     b = ((pt || qf) && (pt && qf)) || ((pt == false && qf == false)
+     c = ((pf || qt) && (pf && qt)) || ((pf == false && qt == false)
+     d = ((pf || qf) && (pf && qf)) || ((pf == false && qf == false)
+
+     System.out.println("T-T: "+a);
+     System.out.println("T-F: "+b);
+     System.out.println("F-T: "+c);
+     System.out.println("F-F: "+d);
+
+
+     a = ((pt || qt) && (pt && qt)) || ((pt || (qt == false)) && (pt && qt));
+     b = ((pt || qf) && (pt && qf)) || ((pt || (qf == false)) && (pt && qf));
+     c = ((pf || qt) && (pf && qt)) || ((pf || (qt == false)) && (pf && qt));
+     d = ((pf || qf) && (pf && qf)) || ((pf || (qf == false)) && (pf && qf));
 
      System.out.println("T-T: "+a);
      System.out.println("T-F: "+b);
