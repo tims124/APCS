1af3a57f97a320a4efeb4bfa06c405f681cf8e70
tims124 <43677159+tims124@users.noreply.github.com>
2018-10-17 11:55:54 -0600

    dfca

--- lab_0/TruthTables.java
+++ lab_0/TruthTables.java
@@ -5,7 +5,7 @@ public class TruthTables {
      boolean qt = true;
      boolean qf = false;
 
-     boolean a = ((pt && qt) || (pt == false));
+     boolean a = ((pt && qt) || (pt == false)); //p1
      boolean b = ((pt && qf) || (pt == false));
      boolean c = ((pf && qt) || (pf == false));
      boolean d = ((pf && qf) || (pf == false));
@@ -14,9 +14,9 @@ public class TruthTables {
      System.out.println("T-F: "+b);
      System.out.println("F-T: "+c);
      System.out.println("F-F: "+d);
+     System.out.println("");
 
-
-     a = ((pt == false) || qt) || (pt && qt);
+     a = ((pt == false) || qt) || (pt && qt); //p2
      b = ((pt == false) || qf) || (pt && qf);
      c = ((pf == false) || qt) || (pf && qt);
      d = ((pf == false) || qf) || (pf && qf);
@@ -25,20 +25,20 @@ public class TruthTables {
      System.out.println("T-F: "+b);
      System.out.println("F-T: "+c);
      System.out.println("F-F: "+d);
+     System.out.println("");
 
-
-     a = ((pt || qt) && (pt && qt)) || ((pt == false && qt == false)
-     b = ((pt || qf) && (pt && qf)) || ((pt == false && qf == false)
-     c = ((pf || qt) && (pf && qt)) || ((pf == false && qt == false)
-     d = ((pf || qf) && (pf && qf)) || ((pf == false && qf == false)
+     a = ((pt || qt) && (pt && qt)) || ((pt == false) && (qt == false)); //p3
+     b = ((pt || qf) && (pt && qf)) || ((pt == false) && (qf == false));
+     c = ((pf || qt) && (pf && qt)) || ((pf == false) && (qt == false));
+     d = ((pf || qf) && (pf && qf)) || ((pf == false) && (qf == false));
 
      System.out.println("T-T: "+a);
      System.out.println("T-F: "+b);
      System.out.println("F-T: "+c);
      System.out.println("F-F: "+d);
+     System.out.println("");
 
-
-     a = ((pt || qt) && (pt && qt)) || ((pt || (qt == false)) && (pt && qt));
+     a = ((pt || qt) && (pt && qt)) || ((pt || (qt == false)) && (pt && qt)); //p4
      b = ((pt || qf) && (pt && qf)) || ((pt || (qf == false)) && (pt && qf));
      c = ((pf || qt) && (pf && qt)) || ((pf || (qt == false)) && (pf && qt));
      d = ((pf || qf) && (pf && qf)) || ((pf || (qf == false)) && (pf && qf));
