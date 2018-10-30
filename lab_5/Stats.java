fb8278f89efa18b4be9f560821e2dd48e211fcc2
tims124 <43677159+tims124@users.noreply.github.com>
2018-10-30 14:05:37 -0600

    10/30

--- lab_5/Stats.java
+++ lab_5/Stats.java
@@ -1,39 +1,50 @@
-
+import java.util.*;
 public class Stats{
   public Stats(){
-      array = new double[count];
+    count = 0;
+
+    C=0;
   }
 
-  public void addData(double a){
-    this.total = this.total + a;
+  public double addData(double a){  //adds data to a list and counts how many numbers are in that list
+    this.total = this.total + a;    //also adds all of the data up
     this.sum = this.sum + Math.pow(a,2);
+    values.add(count,a);            //starts at zero
     this.count = this.count + 1;
-    double array[];
-    array = new double[count];
-    array[count] = a;
+    return a;
   }
 
-  public int getCount(){
+  public int getCount(){    //returns how many numbers are in the list
     return count;
   }
 
-  public double getAverage(){
+  public double getAverage(){ //gets average by dividing total by number of entries
     double average = total / count;
+    this.avg = average;
     return average;
   }
 
-  public double getStandardDeviation(){
+  public double getStandardDeviation(){ //gets standard deviation
+    int counter = 0;
+    double avg = this.getAverage();    //could cut this maybe
 
+    while(counter < count){            //does summation of (x-avg)^2 of the standard deviaiton
+      double A = values.get(counter) - avg; //by pulling all of the numbers out of the list
+      double B = Math.pow(A,2);
+      this.C = B + this.C;
+      counter = counter + 1;
+    }
 
-
-    double buffer = (sum - this.getAverage()) / (count-1);
-    SD = Math.sqrt(buffer);
+    this.SD = Math.sqrt(this.C/(count - 1));  //divides it by the number of entries in the list
     return SD;
   }
 
+
+  private double C;
   private double total;
   private double sum;
   private double SD;
+  private double avg;
   private int count;
-
+  List<Double> values = new ArrayList<Double>();
 }
--- lab_5/StatsTest.java
+++ lab_5/StatsTest.java
@@ -1,24 +1,32 @@
 
+import java.util.*;
 import java.util.Scanner;
 public class StatsTest{
   public static void main(String[] args){
 
+    //setup stuff
     Stats test = new Stats();
     Scanner input = new Scanner(System.in);
+    double A = 0;
+    double B = 0;
+    double C = 0;
+    int counter = 0;
 
+    //finds out how many numbers are going to be entered
     System.out.println("This program will find the average and standard");
     System.out.println("deviation of a list of numbers you will enter.");
     System.out.print("How many values will you enter? ");
     int stop = input.nextInt();
-    int counter = 0;
 
+    //throws those numbers into the class
     while(counter < stop){
-      counter = counter + 1;
       System.out.print("Enter your values one at a time: ");
       double buffer = input.nextDouble();
       test.addData(buffer);
+      counter = counter + 1;
     }
 
+    //output stuff
     System.out.println("You entered "+test.getCount()+" numbers");
 
     System.out.println("The average is " +test.getAverage());
